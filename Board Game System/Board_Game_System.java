3fad6d265b66aeee3d6777d53966568afa6d9a81
Shimaa-78 <90785340+Shimaa-78@users.noreply.github.com>
2023-02-22 18:52:06 +0200

    board class

--- Board Game System/Board_Game_System.java
+++ Board Game System/Board_Game_System.java
@@ -92,3 +92,76 @@ public class Game {
         }
     }
 }
+public class Board{
+    protected int BordSz ;
+    protected char board [][];
+    protected int n_moves = 0;
+    public Board(){}
+    public Board(int sz){
+        BordSz = sz;
+         board  = new char [BordSz][ BordSz];
+    }
+    public boolean updateBoard(int raw,int col,char symbol){
+    if((raw >=0 && raw<=2 && col>=0||col<=2)&&(board[raw][col] == 0)){
+        board[raw][col] = toUpperCase(symbol);
+        n_moves++ ;
+        return true;
+    }
+    else
+        return false;
+  
+    } 
+    public  void display_board(){
+      for (int i=0;i<BordSz;++i) {
+        System.out.print("\n| "); 
+      for (int j=0;j<BordSz;++j) {
+         System.out.print("(" +i + "," +j+ ")"); 
+          System.out.print( " " + board [i][j] + " |");
+    }
+        System.out.print ("\n-----------------------------");
+    }
+        System.out.println("");
+    }
+    public boolean col_win(){
+        for(int i= 0;i<BordSz;++i){ 
+        if((board[0][i]==board[1][i] && board[1][i]==board[2][i])&&board[0][i]!= '\u0000'&&board[1][i]!= '\u0000'&&board[2][i]!= '\u0000')
+        { 
+            return true;   
+        }
+      }
+      return false;
+    }
+    public boolean raw_win(){
+             for(int i= 0;i<BordSz;++i){
+           if(board[i][0]==board[i][1] && board[i][1]==board[i][2]&&board[i][0]!= '\u0000'&&board[i][1]!= '\u0000'&&board[i][2]!= '\u0000')
+            return true;
+               
+             }
+        
+                return false;
+    }
+    public boolean dyg_win(){
+        return ((board[0][0] == board[1][1])&&(board[1][1] == board[2][2])&&
+               (board[0][0]!= '\u0000'&&board[1][1]!= '\u0000'&&board[2][2]!= '\u0000'))
+                ||((board[0][2] == board[1][1])&&(board[1][1] == board[2][0])
+                &&(board[0][2]!= '\u0000'&&board[1][1]!= '\u0000'&&board[2][0]!= '\u0000'));
+    }
+    public  boolean is_winner(){
+        
+         if(col_win()||raw_win()||dyg_win() )
+             return true;
+        
+   return false;
+
+   }
+    public boolean is_draw(){
+        return n_moves == 9 & !is_winner();
+     }
+    public void setBoardsize(int sz){
+        BordSz = sz;
+        board  = new char [BordSz][ BordSz];
+    }
+    public int getBoardSize(){
+        return BordSz;
+    }
+}
